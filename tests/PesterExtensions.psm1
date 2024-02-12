63ce53b45f1d1c8705a0c581d30a6f440cf8dd69
Sietse van der Schoot <sietse.van.der.schoot@enexis.nl>
2024-02-13 00:10:01 +0100

    Updated -BeEquivalentTo to compare arrays irrespective of sorting of arrays.

--- tests/PesterExtensions.psm1
+++ tests/PesterExtensions.psm1
@@ -1,73 +1,83 @@
 #requires -Modules Functional 
 
 Function BeEquivalentTo {
-    param (
-      [PSCustomObject] $ActualValue,
-      [Parameter(ValueFromRemainingArguments)]
-      [PsCustomObject] $Expected,
-      [PsCustomObject] $CallerSessionState,
-      [Switch] $Negate
-    )
+  param (
+    [PSCustomObject] $ActualValue,
+    [Parameter(ValueFromRemainingArguments)]
+    [PsCustomObject] $Expected,
+    [PsCustomObject] $CallerSessionState,
+    [Switch] $Negate
+  )
   
-    $success = (-not $Negate) -eq (Test-IsEquavalentTo @($ActualValue, $Expected))
+  $success = (-not $Negate) -eq (Test-IsEquavalentTo @($ActualValue, $Expected))
   
-    if (!$success) {
+  if (!$success) {
   
-      $failureMessage = $Negate `
-        ? "Expected object not to be equivalent to:`n$(($Expected | ConvertTo-Json))" `
-        : "Expected:`n$(($Expected | ConvertTo-Json))`nBut found: `n$(($ActualValue | ConvertTo-Json))"
-    }
-  
-    [PsCustomObject]@{ Succeeded = $success; FailureMessage = $failureMessage }
+    $failureMessage = $Negate `
+      ? "Expected object not to be equivalent to:`n$(($Expected | ConvertTo-Json))" `
+      : "Expected:`n$(($Expected | ConvertTo-Json))`nBut found: `n$(($ActualValue | ConvertTo-Json))"
   }
   
-  Function ContainEquivalentOf {
-    param (
-      [PSCustomObject[]] $ActualValue,
-      [Parameter(ValueFromRemainingArguments)]
-      [PsCustomObject] $Expected,
-      [PsCustomObject] $CallerSessionState,
-      [Switch] $Negate
-    )
+  [PsCustomObject]@{ Succeeded = $success; FailureMessage = $failureMessage }
+}
   
-    $isMatch = $false
+Function ContainEquivalentOf {
+  param (
+    [PSCustomObject[]] $ActualValue,
+    [Parameter(ValueFromRemainingArguments)]
+    [PsCustomObject] $Expected,
+    [PsCustomObject] $CallerSessionState,
+    [Switch] $Negate
+  )
+  
+  $isMatch = $false
     
-    foreach ($item in $ActualValue) {
+  foreach ($item in $ActualValue) {
       
-      if ((Test-IsEquavalentTo @($item, $Expected))) {
+    if ((Test-IsEquavalentTo @($item, $Expected))) {
   
-        $isMatch = $true
-        break;
-      }
+      $isMatch = $true
+      break;
     }
-  
-    $success = (-not $Negate) -eq $isMatch
-  
-    if (!$success) {
-  
-      $failureMessage = $Negate `
-        ? "Expected collection not to contain:`n$(($Expected | ConvertTo-Json))" `
-        : "Expected collection to contain:`n$(($Expected | ConvertTo-Json))`nBut found: `n$(($ActualValue | ConvertTo-Json))"
-    }
-  
-    [PsCustomObject]@{ Succeeded = $success; FailureMessage = $failureMessage }
   }
-
+  
+  $success = (-not $Negate) -eq $isMatch
+  
+  if (!$success) {
+  
+    $failureMessage = $Negate `
+      ? "Expected collection not to contain:`n$(($Expected | ConvertTo-Json))" `
+      : "Expected collection to contain:`n$(($Expected | ConvertTo-Json))`nBut found: `n$(($ActualValue | ConvertTo-Json))"
+  }
+  
+  [PsCustomObject]@{ Succeeded = $success; FailureMessage = $failureMessage }
+}
 
 Function Test-IsEquavalentTo {
   param(
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
-    [object[]] $Object
+    [object[]] $Objects
   )
 
-  if ((Test-IsArrayOfValueTypes $Object | Test-All)) {
+  # Compare two arrays with valueTypes or strings, types which can be sorted. Sort
+  # before comparing. Order is irrelevant in our tests.
+  if ($Objects | ForEach-Object { $item = $_; Test-IsArrayOfValueTypes $item } | Test-All) {
 
-    $Object | ForEach-Object { (@($_) | Sort-Object) } | Test-Equality
-
+    $Objects | ForEach-Object { ,($_ | Sort-Object) }  | Test-Equality
   }
   else {
-    $Object | Test-Equality
+
+    # One of the arguments is an array, the other is not. Some the times it is comparing an object 
+    # with an array containing a single item. Flatten the arrays before use.
+    if ($Objects | ForEach-Object { $_ -is [array] } | Reduce-Object { param($a, $b) $a -xor $b }) {
+
+      $Objects | ForEach-Object { $_ } | Test-Equality
+    }
+    else {
+    
+      $Objects | Test-Equality
+    }
   }
 } 
 
@@ -76,9 +86,9 @@ Function Test-IsArrayOfValueTypes {
     [object] $Object 
   )
 
-  $object -is [array] -and (@($object) | ForEach-Object { $_.GetType().IsValueType } | Test-All )
+  $isArrayOfStringOrValueTypes = ($object -is [array] -and (@($object) | ForEach-Object { $_.GetType().IsValueType -or $_ -is [string] } | Test-All ))
+  $null -ne $Object -and $isArrayOfStringOrValueTypes
 }
-
 
 Export-ModuleMember -Function BeEquivalentTo
 Export-ModuleMember -Function ContainEquivalentOf
